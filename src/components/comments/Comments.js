cd52ddda5dfee51034947868488103f1a8de4bb4
burhan orkun <beyazmartilar@gmail.com>
2021-06-25 15:35:28 +0300

    added comment request

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,49 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from './CommentsList';
+import { useParams } from 'react-router';
 
 const Comments = () => {
     const [isAddingComment, setIsAddingComment] = useState(false);
+    const params = useParams();
+
+    const { quoteId } = params;
+    const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [quoteId, sendRequest]);
 
     const startAddCommentHandler = () => {
         setIsAddingComment(true);
     };
+
+    const addedCommentHandler = useCallback(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
+
+    let comments;
+
+    if (status === 'pending') {
+        comments = (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    if (status === 'completed' && loadedComments && loadedComments.length > 0) {
+        comments = <CommentsList comments={loadedComments} />
+    }
+
+    if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+        comments = <p className='centered'>No comments were added yet!</p>
+    }
 
     return (
         <section className={classes.comments}>
@@ -18,8 +53,10 @@ const Comments = () => {
                     Add a Comment
                 </button>
             )}
-            {isAddingComment && <NewCommentForm />}
-            <p>Comments...</p>
+            {isAddingComment &&
+                <NewCommentForm quoteId={quoteId} onAddedComment={addedCommentHandler} />
+            }
+            {comments}
         </section>
     );
 };
